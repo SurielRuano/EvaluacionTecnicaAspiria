d293eadf6879328278e14268ce8d42907cc853d4
Suriel <sirleirus@hotmail.com>
2021-05-06 21:43:11 -0500

    Integracion de proyecto para frontend, creacion de servicio para consumo del API

---
+++ JugueteriaFrontend/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using JugueteriaFrontend.Helper;
+using JugueteriaFrontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JugueteriaFrontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        ProductosAPI _apiProductos = new ProductosAPI();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Producto> productos = new List<Producto>();
+            HttpClient client = _apiProductos.Initial();
+            HttpResponseMessage response = await client.GetAsync("Productos");
+
+            if (response.IsSuccessStatusCode) 
+            {
+                var results = response.Content.ReadAsStringAsync().Result;
+                productos = JsonConvert.De
+            }
+            var productos = new RequestToApi().ObtenerProductos();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JugueteriaFrontend/Helper/ProductosAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JugueteriaFrontend.Helper
+{
+    public class ProductosAPI
+    {
+        public HttpClient Initial() {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:44328/");
+            return client;
+        }        
+    }
+}
